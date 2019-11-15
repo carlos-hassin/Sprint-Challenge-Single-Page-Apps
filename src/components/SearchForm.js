185eb75c83e42ecc94698ad96d4ec83e51d0a9e8
cehassin <cehassin@gmail.com>
2019-11-15 10:48:08 -0800

    Steps 1 & 2 done, on the last

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,15 @@ export default function SearchForm() {
  
   return (
     <section className="search-form">
-     // Add a search form here
+     <form onSubmit={() => onSearch(name)}>
+        <input
+          onChange={handleInputChange}
+          placeholder="name"
+          value={name}
+          name="name"
+        />
+        <button type="submit">Search</button>
+      </form>
     </section>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const CharacterLink = styled.div`
+text-align:center;
+height:20px;
+width:150px;
+background-color:grey;
+border-radius: 50px;
+
+`;
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        />
+          <div className="link_conatiner">
+            <CharacterLink>
+              <Link to="/characters" className="link_characters"> View Characters</Link>
+            </CharacterLink>          
+          </div>
       </header>
     </section>
   );
